bcacd808932e6ef98cd81f0a8ac8f78c0f43ad0b
mckeeh3 <mckeeh3@gmail.com>
2022-12-02 06:39:22 -0500

    add action

---
+++ src/main/java/io/woe/GeneratorToGeneratorAction.java
@@ -0,0 +1,24 @@
+package io.woe;
+
+import kalix.javasdk.action.Action;
+import kalix.springsdk.KalixClient;
+import kalix.springsdk.annotations.Subscribe;
+
+public class GeneratorToGeneratorAction extends Action {
+  private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(GeneratorToGeneratorAction.class);
+  private final KalixClient kalixClient;
+
+  public GeneratorToGeneratorAction(KalixClient kalixClient) {
+    this.kalixClient = kalixClient;
+  }
+
+  @Subscribe.EventSourcedEntity(GeneratorEntity.class)
+  public Effect<String> on(GeneratorEntity.GeneratedEvent event) {
+    log.info("Event: {}", event);
+    var path = "/generator/%s/generate".formatted(event.generatorId());
+    var command = new GeneratorEntity.GenerateCommand(event.generatorId());
+    var deferredCall = kalixClient.put(path, command, String.class);
+
+    return effects().forward(deferredCall);
+  }
+}
